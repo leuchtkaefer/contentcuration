acb894a4ee20c0ef3170cb6332b8f775320b6f22
leuchtkaefer <vcjeg@yahoo.com>
2013-09-22 01:23:56 +0200

    cosmetic changes, documentation

--- ContentCuration/src/plugins/ccuration/LibraryTalker.java
+++ ContentCuration/src/plugins/ccuration/LibraryTalker.java
@@ -1,23 +1,31 @@
+/* Curator, Freenet plugin to curate content
+ * Copyright (C) 2013 leuchtkaefer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 package plugins.ccuration;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
 
 import plugins.ccuration.fcp.wot.WoTOwnIdentities;
 import plugins.ccuration.index.InputEntry;
 import plugins.ccuration.index.TermEntry;
 import plugins.ccuration.index.TermEntryWriter;
-import plugins.ccuration.index.TermFileEntry;
-import plugins.ccuration.index.TermPageEntry;
 import freenet.keys.FreenetURI;
 import freenet.pluginmanager.FredPluginTalker;
 import freenet.pluginmanager.PluginNotFoundException;
@@ -27,14 +35,12 @@ import freenet.support.Base64;
 import freenet.support.Logger;
 import freenet.support.SimpleFieldSet;
 import freenet.support.api.Bucket;
-import freenet.support.io.FileBucket;
 
 /**
- * Buffer which stores TermPageEntrys as they are submitted by one unique WoT Identity. When the
- * buffer's estimated size gets up to bufferMax, the buffer is serialized into a
- * Bucket and sent to the Library...//TODO leuchtkaefer not sure if it will work on this way
+ * It prepares a buffer to keep TermPageEntries and TermFileEntries as they are submitted by one unique WoT Identity. 
+ * The buffer is immediately serialized into a Bucket and sent to the Library. 
+ * It is a simplified version of LibraryBuffer for Spider (@author MikeB).
  * 
- *
  * @author leuchtkaefer
  */
 
@@ -43,16 +49,16 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 
 	private PluginRespirator pr;
 	private boolean shutdown;
-	
+
 	private ContentCuration cCur; 
-		
+
 	LibraryTalker(PluginRespirator pr, ContentCuration cCur) {
 		this.pr = pr;
 		this.cCur = cCur;
 	}
-	
-	
-	
+
+
+
 	/**
 	 * 
 	 * @param input
@@ -69,10 +75,10 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 		synchronized(this) {
 			if(shutdown) return;
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * It gets all identities and send a message to Library in order to finalize any pending on disk merging job. 
 	 * @throws PluginNotFoundException
@@ -81,17 +87,17 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 		PluginTalker libraryTalker;
 		SimpleFieldSet sfs = new SimpleFieldSet(true);
 		sfs.putSingle(COMMAND, PUSH_BUFFER);
-		
+
 		//Get all identities
 		Map<String, String> identityList = WoTOwnIdentities.getWoTIdentities("InsertURI");
-		
+
 		libraryTalker = pr.getPluginTalker(this, PLUGINS_LIBRARY_MAIN, CONTENT_CURATOR);
-		
+
 		//Make sure there is at least one identity
 		if (identityList.isEmpty()) {
 			return;
 		}
-		
+
 		Iterator<String> WoTId = identityList.keySet().iterator();
 		FreenetURI insertURI;
 		while (WoTId.hasNext()) {
@@ -110,14 +116,14 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 				} catch (PluginNotFoundException e) {
 					Logger.error(this, "WoT plugin not found", e);
 				} catch (MalformedURLException e) {
-				Logger.error(this,"MalformedURL insertURI" + identityID, e);
+					Logger.error(this,"MalformedURL insertURI" + identityID, e);
 				}	
 			}
 		}
-		
+
 	}
-	
-	
+
+
 	private void innerSend(Bucket bucket, InputEntry input) {
 		System.out.println("Leuchtkaefer master of curator, I reached innerSend");
 		System.out.println("Leuchtkaefer input size "+ input.getTpe().size());
@@ -126,16 +132,7 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 		sfs.putSingle(INSERT_URI, input.getPrivKey().toASCIIString());
 		sfs.putSingle(HASH_PUBKEY, Base64.encode(input.getPubKey().getRoutingKey()));
 		sfs.putSingle(INDEX_NAME, input.getPubKey().getDocName());
-		/*
-		InputStream is = null; //TODO leuchtkaefer remove all this try
-		try{
-			is = bucket.getInputStream();
-			SimpleFieldSet fs = new SimpleFieldSet(new LineReadingInputStream(is), 1024, 512, true, true, true);
-		} catch (IOException ex) {
-			Logger.error(this,"IO Exception", ex);
-		} finally {
-			Closer.close(is);
-		}*/
+
 		PluginTalker libraryTalker;
 		try {
 			libraryTalker = pr.getPluginTalker(this, PLUGINS_LIBRARY_MAIN, CONTENT_CURATOR);
@@ -146,43 +143,37 @@ public class LibraryTalker implements FredPluginTalker, ContentCurationConstants
 		}
 	}
 
+	/**
+	 * Fills a bucket with index'metadata and the TermEntries contain in InputEntry. TermEntries are related to 
+	 * one unique Freenet object, that could be a web page or a file (pdf, jpg, etc).
+	 * 
+	 * @param bucket
+	 * @param input
+	 * @throws IOException
+	 */
 	private void fillBucket(Bucket bucket, InputEntry input) throws IOException {
-		System.out.println("Leuchtkaefer master of curator, I reached fillBucket");
-		
 		OutputStream os = bucket.getOutputStream();
 		SimpleFieldSet meta = new SimpleFieldSet(true); // Stored with data to make things easier.
 		String indexOwner = Base64.encode(input.getPubKey().getRoutingKey());
-		
-		meta.putSingle("index.title", input.getPrivKey().getDocName());
+		meta.putSingle("index.title", input.getPubKey().getDocName());
 		if(indexOwner != null) {
 			meta.putSingle("index.owner.name", indexOwner);
 		}
 		meta.putSingle("index.owner.email", "private");
 		meta.writeTo(os);
-/*	
-		if (input.getInputType()==TermEntry.EntryType.FILE) {
-			for (TermFileEntry termFileEntry : input.getTpe()) {
-				TermEntryWriter.getInstance().writeObject(termFileEntry, os); //TODO leuchtkaefer I need to modify TermEntry to accept FIL
-			}	
-		} else if (input.getInputType()==TermEntry.EntryType.PAGE) {
-			for (TermPageEntry termPageEntry : input.getTpe()) {
-				TermEntryWriter.getInstance().writeObject(termPageEntry, os); 
-			}
-		}
-*/			
+
 		for (TermEntry termEntry : input.getTpe()) {
-			TermEntryWriter.getInstance().writeObject(termEntry, os); //TODO leuchtkaefer I need to modify TermEntry to accept FIL
+			TermEntryWriter.getInstance().writeObject(termEntry, os); 
 		}
 		os.close();
 		bucket.setReadOnly();
-		System.out.println("Leuchtkaefer master of curator, fillBucket ended");
 	}
-	
+
 	@Override
 	public void onReply(String pluginname, String indentifier,
 			SimpleFieldSet params, Bucket data) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
