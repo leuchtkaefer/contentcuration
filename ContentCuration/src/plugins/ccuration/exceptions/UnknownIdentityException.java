48d6109bd270431af9e9bd6b58daabfbed17aefa
leuchtkaefer <vcjeg@yahoo.com>
2013-09-22 01:26:16 +0200

    documentation

--- ContentCuration/src/plugins/ccuration/exceptions/UnknownIdentityException.java
+++ ContentCuration/src/plugins/ccuration/exceptions/UnknownIdentityException.java
@@ -1,8 +1,18 @@
-/**
- * This code is part of WoT, a plugin for Freenet. It is distributed 
- * under the GNU General Public License, version 2 (or at your option
- * any later version). See http://www.gnu.org/ for details of the GPL.
- */
+/* Curator, Freenet plugin to curate content
+ * Copyright (C) 2013 leuchtkaefer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 package plugins.ccuration.exceptions;
 
 /**
